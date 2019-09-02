e9d0e3cb957130879da2d9837bf03e3489c23e4d
Jens Gerntholtz <jens@io.co.za>
2019-09-02 14:57:38 +0200

    ship asset

--- spaceScroller.js
+++ spaceScroller.js
@@ -1,9 +1,9 @@
-console.log('spaceScroller.js');
+console.log("spaceScroller.js");
 var count = 20;
 
 var path = new Path.Rectangle({
   size: [5, 30],
-  fillColor: 'white'
+  fillColor: "white"
 });
 
 var symbol = new Symbol(path);
@@ -15,20 +15,33 @@ for (var i = 0; i < count; i++) {
   // The center position is a random point in the view:
   var center = Point.random() * view.size;
   var placedSymbol = symbol.place(center);
-  placedSymbol.scale((i / count * (max - min)) + min);
+  placedSymbol.scale((i / count) * (max - min) + min);
 }
 
-var player = new Path.Rectangle({
+var detectionPath = new Path.Rectangle({
   point: view.center,
   size: [80, 100],
-  fillColor: 'white'
+  // fillColor: 'white'
 });
+
+console.log("svg?", detectionPath);
+var player = new Group([detectionPath]);
+player.importSVG(
+  "./src/cleanHornet.svg",
+  {
+    onLoad: function(item) {
+      item.center = view.center;
+      item.scale(0.1, 0.1);
+      item.position = detectionPath.position;
+      svgItem = item;
+    }
+  }
+);
+console.log("player", player);
 
 var tempo = 10;
 
-
 function onFrame(event) {
-  
   for (var i = 0; i < count; i++) {
     var item = project.activeLayer.children[i];
 
@@ -38,11 +51,13 @@ function onFrame(event) {
       item.position.y = -item.bounds.height;
     }
 
-    if (player.intersects(item)) {
-      var playerDistance = player.position.getDistance(item.position)
+    if (detectionPath.intersects(item)) {
+      var detectionPathDistance = detectionPath.position.getDistance(
+        item.position
+      );
 
-      player.position.x += playerDistance / (player.position.x - item.position.x);
+      player.position.x +=
+        detectionPathDistance / (detectionPath.position.x - item.position.x);
     }
-
   }
 }
