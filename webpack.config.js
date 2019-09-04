b048f98e4595841ae995febd2f06195920b1bc59
Jens Gerntholtz <jens@io.co.za>
2019-09-04 07:43:49 +0200

    asset loader webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,19 +5,27 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: {
-    app: "./src/app.js",
+    app: "./src/app.js"
     // print: "./src/print.js"
   },
   devtool: "inline-source-map",
   plugins: [
     // new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: "./index.html",
       title: "Development"
     })
   ],
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      }
+    ]
   }
 };
